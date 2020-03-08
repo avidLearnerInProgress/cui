ff8709a8ea4bd18ec1e4553607050620659ca925
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-03-08 00:43:44 +0000

    Added solution

--- practice/c++/sieve_of_eranthoses.cpp
+++ practice/c++/sieve_of_eranthoses.cpp
@@ -1,8 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define sieveOf(n) (vector<bool> v(n+1); fill(v.begin(), v.end(), true); for(int i=2; i<sqrt(n); i++) if(v[i]) for(int j=i*i; j<=n; j+=i) v[j]= false; for(int i=2; i<=n; i++) if(v[i]) cout<<i<<"\n";)
-
 void sieve(int n){
     vector<bool> v(n+1);
     fill(v.begin(), v.end(), true);
